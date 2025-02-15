f2e0df6b5b3d8f5fb7bd000b72e38b853f135edd
Bhanu Prakash Poluparthi <bhanuprakash@bhanus-mbp.mynetworksettings.com>
2025-02-14 21:08:09 -0500

    Add unit integrations and e2e tests

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -11,14 +11,3 @@ def test_subtract():
 def test_multiply():
     calc = Calculator()
     assert calc.multiply(2, 3) == 6
-
-def test_divide():
-    calc = Calculator()
-    assert calc.divide(6, 3) == 2
-
-def test_divide_by_zero():
-    calc = Calculator()
-    try:
-        calc.divide(6, 0)
-    except ValueError as e:
-        assert str(e) == "Cannot divide by zero"
