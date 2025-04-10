8e77cb30bfcc0481bdd4183a74b908f2404fb443
Rhuthvik Dendukuri <85996346+Rhuthvik-D@users.noreply.github.com>
2025-04-10 18:25:04 -0400

    Update __init__.py

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,5 +1,5 @@
-from calculator import Calculator
-from logger import Logger
-from notifier import Notifier
+from .calculator import Calculator
+from .logger import Logger
+from .notifier import Notifier
 
 __all__ = ["Calculator", "Logger", "Notifier"]
